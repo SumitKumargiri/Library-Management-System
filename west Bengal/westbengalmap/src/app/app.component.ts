7c1c078b23161582e6b9bf841cc026e43b22a984
SumitKumargiri <sumitgiri15434@gmail.com>
2025-07-05 21:32:35 +0530

    first

---
+++ west Bengal/westbengalmap/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  // districts = [
+  //   { name: 'Alipurduar', x: 10, y: 5 },
+  //   { name: 'Bankura', x: 40, y: 50 },
+  //   { name: 'Bardhaman (Purba Bardhaman, Paschim Bardhaman)', x: 55, y: 55 },
+  //   { name: 'Birbhum', x: 30, y: 45 },
+  //   { name: 'Cooch Behar', x: 5, y: 10 },
+  //   { name: 'Dakshin Dinajpur', x: 20, y: 30 },
+  //   { name: 'Darjeeling', x: 3, y: 5 },
+  //   { name: 'Hooghly', x: 60, y: 65 },
+  //   { name: 'Howrah', x: 75, y: 70 },
+  //   { name: 'Jalpaiguri', x: 5, y: 10 },
+  //   { name: 'Jhargram', x: 35, y: 50 },
+  //   { name: 'Kalimpong', x: 5, y: 5 },
+  //   { name: 'Kolkata', x: 70, y: 65 },
+  //   { name: 'Malda', x: 10, y: 25 },
+  //   { name: 'Murshidabad', x: 60, y: 40 },
+  //   { name: 'Nadia', x: 70, y: 50 },
+  //   { name: 'North 24 Parganas', x: 85, y: 60 },
+  //   { name: 'Paschim Medinipur', x: 40, y: 50 },
+  //   { name: 'Purba Medinipur', x: 50, y: 60 },
+  //   { name: 'Purulia', x: 20, y: 30 },
+  //   { name: 'South 24 Parganas', x: 50, y: 80 },
+  //   { name: 'Uttar Dinajpur', x: 15, y: 15 }
+  // ];
+  
+}
